9b943cca9d823818351f5d89510514ddeebfd354
duchieu <duchieu.ctk41@gmail.com>
2021-03-08 09:57:16 +0700

    16/7 && 17/7 sign-up && sign-in with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.ultis";
+import { auth, signInWithGoogle } from "../../firebase/firebase.ultis";
 
 class SignIn extends Component {
   constructor() {
@@ -16,10 +16,15 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-
-    this.setState({ email: "", password: "" })
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   handleChange = event => {
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -28,10 +28,11 @@ class SignUp extends Component {
     }
 
     try {
-      const { user } = await auth.createUserWithEmailAndPassword({
+      const { user } = await auth.createUserWithEmailAndPassword(
         email,
         password
-      });
+      );
+      console.log({user})
 
       await createUserProfileDocument(user, { displayName });
 
@@ -55,14 +56,14 @@ class SignUp extends Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
-      <div>
+      <div className="sign-up">
         <h1>I do not have an account</h1>
         <span>Sign up your email and password</span>
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
         <FormInput type="text" name="displayName" value={displayName} label="display Name" onChange={this.handleChange} required ></FormInput>
         <FormInput type="email" name="email" value={email} label="Email" onChange={this.handleChange} required ></FormInput>
         <FormInput type="password" name="password" value={password} label="Password" onChange={this.handleChange} required ></FormInput>
-        <FormInput type="confirmPassword" name="confirmPassword" value={confirmPassword} label="Confirm Password" onChange={this.handleChange} required ></FormInput>
+        <FormInput type="password" name="confirmPassword" value={confirmPassword} label="Confirm Password" onChange={this.handleChange} required ></FormInput>
         <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
       </div>
